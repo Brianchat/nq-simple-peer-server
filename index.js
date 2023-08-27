c68d74332069825e60975d7d3d00a835968fe153
Brian Chatterton <brian.chatterton@gmail.com>
2023-08-27 21:05:17 +0100

    Test

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const http = require('http');
-const SimplePeerServer = require('simple-peer-server');
+// const http = require('http');
+// const SimplePeerServer = require('simple-peer-server');
 
-const server = http.createServer();
-const spServer = new SimplePeerServer(server, true);
+// const server = http.createServer();
+// const spServer = new SimplePeerServer(server, true);
 
-server.listen(8081);
+// server.listen(8081);
+
+
+// require expressjs
+const express = require("express")
+const app = express()
+// define port 8080
+PORT = 8081
+app.use(express.json())
+// use router to bundle all routes to /
+const router = express.Router()
+app.use("/", router)
+// get on root route 
+router.get("/", (req,res) => {
+	res.send("hello world!")
+})
+// start server
+app.listen(PORT, () => {
+	console.log("Server is up and running!!")
+})
