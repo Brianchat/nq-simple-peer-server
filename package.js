c6e43d5a1ff00522457c75018e82de69fdc85d55
Brianchat <brian.chatterton@gmail.com>
2023-08-27 19:58:22 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "simple-peer-server-example",
+  "description": "An example of a signaling server using simple-peer-server.",
+  "version": "0.0.11",
+  "scripts": {
+    "start": "node index.js",
+    "watch": "nodemon --inspect index.js"
+  },
+  "dependencies": {
+    "simple-peer-server": ""
+  },
+  "devDependencies": {
+    "nodemon": "^2.0.12"
+  }
+}
